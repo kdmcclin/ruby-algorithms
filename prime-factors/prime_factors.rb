1d338e8f4c005304f24bf055c578d9e7e70019cf
Katherine McClintic <kdmcclin@gmail.com>
2015-05-19 16:22:33 -0400

    Add prime factors

---
+++ prime-factors/prime_factors.rb
@@ -0,0 +1,20 @@
+#Note: you may choose to use the prime module here.  It is possible to solve either way.
+
+def prime_factors(number)
+	answer = []
+	arrays = number.prime_division
+	arrays.each do |number, times|
+		times.times do answer << number
+		end
+	end
+	answer
+end
+
+
+
+#Tests--DO NOT CHANGE
+p prime_factors(3)         # => [3]
+p prime_factors(6)         # => [2,3]
+p prime_factors(8)         # => [2,2,2]
+p prime_factors(25)        # => [5,5]
+p prime_factors(123123123) # => [3, 3, 41, 333667]
