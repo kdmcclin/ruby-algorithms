8d30bcedc87451b6c5ab2e1063ad2f2ae2357854
Katherine McClintic <kdmcclin@gmail.com>
2015-05-19 17:43:47 -0400

    Add fibonacci files

---
+++ fibonacci/fibonacci.rb
@@ -0,0 +1,13 @@
+def fibonacci_iterative(n)
+	#code
+end
+
+def fibonacci_recursive(n)
+	#code
+end
+
+
+p fibonacci_iterative(0) == 0
+p fibonacci_recursive(1) == 1
+p fibonacci_iterative(2) == 1
+p fibonacci_recursive(20) == 6765
