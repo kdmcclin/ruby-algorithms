b61e0002bc97c8a40cf44473935b385cfa6e2931
Katherine McClintic <kdmcclin@gmail.com>
2015-05-20 20:59:42 -0400

    Add linear search documents

---
+++ linear-search/linear-search.rb
@@ -0,0 +1,16 @@
+def linear_search(object, array)
+	#code
+end
+
+def global_linear_search(query, array)
+	#code
+end
+
+#Tests
+p linear_search(3, [1,2,3])
+p linear_search(4, [1,2,3])
+p linear_search(13, [1,2,3])
+
+bananas_arr = "bananas".split("")
+p global_linear_search("a", bananas_arr)
+p global_linear_search("d", bananas_arr)
