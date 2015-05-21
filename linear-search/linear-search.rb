547f119ffa54404cf745402931ecfbfe37f9263d
Katherine McClintic <kdmcclin@gmail.com>
2015-05-20 21:05:42 -0400

    Update linear search tests

--- linear-search/linear-search.rb
+++ linear-search/linear-search.rb
@@ -7,10 +7,10 @@ def global_linear_search(query, array)
 end
 
 #Tests
-p linear_search(3, [1,2,3])
-p linear_search(4, [1,2,3]) 
-p linear_search(13, [1,2,3])
+p linear_search(3, [1,2,3]) == 2
+p linear_search(4, [1,2,3]) == nil
+p linear_search(13, [1,2,3]) == nil
 
 bananas_arr = "bananas".split("")
-p global_linear_search("a", bananas_arr)
-p global_linear_search("d", bananas_arr)
+p global_linear_search("a", bananas_arr) == [1, 3, 5]
+p global_linear_search("d", bananas_arr) == nil
