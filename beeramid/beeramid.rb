1a7e09747d944c4f366d3b011bda158e27337290
Katherine McClintic <kdmcclin@gmail.com>
2015-06-16 23:35:10 -0400

    Add beeramid master branch

---
+++ beeramid/beeramid.rb
@@ -0,0 +1,12 @@
+def beeramid(bonus, price_per_can)
+	#code here
+end
+
+#consider writing more methods here to break down the code
+
+
+##Tests
+
+p beeramid(9, 2) == 1
+p beeramid(455, 5) == 6
+p beeramid(21, 1.5) == 3
