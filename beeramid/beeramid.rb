baa78d6791b0747197e17e13a4e84516ff3b62b8
Katherine McClintic <kdmcclin@gmail.com>
2015-06-16 23:33:22 -0400

    Add beeramid solution branch

---
+++ beeramid/beeramid.rb
@@ -0,0 +1,37 @@
+#pseudocode
+# one method: how many cans of beer your bonus can buy based on the price
+# one method: add levels of cans 
+# one method: run the beeramid
+	#from one up to the square root of num_cans, run total_cans
+	#when total_cans equals square root of num_cans, break and return level
+	#else when total_cans > square root of num_cans, break and return level-1
+
+
+def beeramid(bonus, price_per_can)
+	cans = num_cans(bonus, price_per_can)
+	square_root = Math.sqrt(cans).floor
+	(1..square_root).each do |n|
+		if total_cans(n) == cans
+			return n
+		elsif total_cans(n) > cans
+			return n - 1 
+		end
+	end
+end
+
+def num_cans(bonus, price_per_can)
+	bonus/price_per_can
+end
+
+def total_cans(level)
+	total = 0
+	level.downto(1) {|level| total += (level**2)}
+	total
+end
+
+
+##Tests
+
+p beeramid(9, 2) == 1
+p beeramid(455, 5) == 6
+p beeramid(21, 1.5) == 3
